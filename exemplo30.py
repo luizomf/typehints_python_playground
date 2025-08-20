e5fce2ded49ecdabc38cbbac9bf07417d4da097e
Luiz Otávio <luizomf@gmail.com>
2025-08-20 10:57:55 -0300

    date maker created with first ideia

---
+++ exemplo30.py
@@ -0,0 +1,134 @@
+import re
+from datetime import UTC, datetime
+from typing import Protocol, overload
+
+from utils import cyan_print, sep_print
+
+################################################################################
+#
+# TypeVar com callback protocol
+#
+# Também é possível usar `TypeVar` com protocols. Isso permite que você faça a
+# assinatura da sua função de forma mais dinâmica. Assim, os tipos podem variar
+# de acordo com o contexto.
+# No exemplo abaixo, tenho um Protocol super simples. A intenção é receber sempre
+# um atributo nomeado chamado `value` com tipo `T` e retornar `R`.
+# Ambos `T` e `R` são `TypeVars` ou Type Parameters dinâmicos.
+#
+################################################################################
+
+# Essa expressão regular será usada para limpar vírgulas.
+# Ela seleciona o seguinte:
+# - `\s*` - zero ou mais espaços
+# - `,` - a vírgula
+# - `\s*` - zero ou mais espaços
+# Tenho curso grátis sobre isso aqui:
+# https://www.youtube.com/playlist?list=PLbIBj8vQhvm1VnTa2Np5vDzCxVtyaYLMr
+RE_COMMA_SPACE = re.compile(r"\s*,\s*")
+
+
+################################################################################
+
+# Vamos definir o nosso `Protocol` que aceita tipos parametrizados (`TypeVar`).
+
+
+class TypeCaster[T, R](Protocol):
+    def __call__(self, *, value: T) -> R: ...
+
+
+################################################################################
+
+# Agora podemos definir nossas funções que cumprem o contrato.
+
+
+def to_str(*, value: object) -> str:
+    """Recebe qualquer coisa e converte em string"""
+
+    return str(value)
+
+
+def str_to_list(*, value: str) -> list[str]:
+    """Recebe uma string e tenta converter em lista"""
+
+    clean_value = RE_COMMA_SPACE.sub(value, ",")
+    return [v.strip() for v in RE_COMMA_SPACE.split(clean_value) if v.strip()]
+
+
+def wrong_kw_name(text: str) -> str:
+    """❌ Esse argumento nomeado está com nome errado"""
+
+    return text
+
+
+################################################################################
+
+# Por fim vamos definir algo que "usa" o nosso `Protocol` e testamos se nossas
+# funções passam na tipagem. Perceba que aqui estamos realmente usando as nossas
+# funções como callback (fazendo o nome callback protocol valer).
+
+
+def run_type_caster[T, R](value: T, type_caster: TypeCaster[T, R]) -> R:
+    """Recebe um valor, um type_caster e executa tudo"""
+
+    return type_caster(value=value)
+
+
+class DateMaker(Protocol):
+    @overload
+    def __call__(self, value: str, *, in_format: str | None = None) -> datetime: ...
+    @overload
+    def __call__(
+        self,
+        value: tuple[int, ...],
+        *,
+        in_format: None = None,
+    ) -> datetime: ...
+    @overload
+    def __call__(self, value: float, *, in_format: None = None) -> datetime: ...
+
+
+@overload
+def date_maker(value: str, in_format: str) -> datetime: ...
+@overload
+def date_maker(value: tuple[int, ...], in_format: None = None) -> datetime: ...
+@overload
+def date_maker(value: float, in_format: None = None) -> datetime: ...
+def date_maker(
+    value: str | tuple[int, ...] | float,
+    in_format: str | None = None,
+) -> datetime:
+    if isinstance(value, str):
+        if not in_format:
+            msg = f"You must inform the date format for {value!r}"
+            raise ValueError(msg)
+
+        return datetime.strptime(value, in_format)
+
+    if isinstance(value, tuple):
+        if not value:
+            msg = "Cannot create a date with empty tuple"
+            raise ValueError(msg)
+
+        return datetime(*value, tzinfo=UTC)
+
+    return datetime.fromtimestamp(value)
+
+
+################################################################################
+
+# Bora testar tudo
+
+
+if __name__ == "__main__":
+    sep_print()
+    date1 = datetime(1, 1, 1)
+    date2 = datetime.strptime("2025-08", "%Y-%m")
+
+    date3 = datetime.fromtimestamp(1755696515.035976)
+
+    cyan_print(f"{date1 = }")
+    cyan_print(f"{date2 = }")
+    cyan_print(f"{date3 = }")
+
+
+################################################################################
