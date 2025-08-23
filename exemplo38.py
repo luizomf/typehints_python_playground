78614516fd62bd1649d7922f640d6cb73eafcfba
Luiz Otávio <luizomf@gmail.com>
2025-08-23 13:04:24 -0300

    type guard first class

--- exemplo38.py
+++ exemplo38.py
@@ -8,7 +8,7 @@ from utils import cyan_print, red_print, sep_print
 #
 # TypeGuard - Precisamos de Guarda-costas?
 #
-# Obs.: PEP 742 e 647 falam de TypeGuard e TypeIs. Focaremos em TypeGuard.
+# Obs.: PEP 742 e 647 falam de TypeIs e TypeGuard. Vamos focar em TypeGuard.
 # Depois TypeIs.
 #
 # Comparação: No TypeScript, funções que fazem Type Guard, são chamadas de
@@ -16,14 +16,21 @@ from utils import cyan_print, red_print, sep_print
 # "Type Narrowing" (ou afunilamento de tipo), assim como `isinstance` ou
 # condicionais que afunilam tipos no Python.
 #
-# TypeGuard[T] e TypeIs[T] são ambas funções para "Type Predicate" no Python.
+# TypeGuard[T] e TypeIs[T] são ambas funções de "Type Predicate" no Python.
 # As duas informam ao Type Checker que se seu retorno for verdadeiro, o tipo do
 # objeto avaliado é `T`.
 #
-# Com a inclusão de TypeIs[T], TypeGuard[T] tenderá a ser menos usado, mas de
-# acordo com a PEP 742 (sobre TypeIs), TypeGuard[T] vai continuar na linguagem.
+# Como TypeGuard Funciona?
+#
+# - TypeGuard[T], aceita um tipo e é usado para anotar o retorno de uma função.
+# - Se o retorno da função for verdadeiro, o tipo é afunilado para `T`.
+# - Se o retorno for falso, o tipo se mantem o mesmo recebido pela função.
 #
 ################################################################################
+
+
+def is_str(value: object) -> TypeGuard[str]:
+    return isinstance(value, str)
 
 
 class UserDict(TypedDict):
@@ -38,7 +45,7 @@ def is_valid_dict(item: object) -> TypeGuard[UserDict]:
 
     item = cast("dict[str, Any]", item)  # isso é para o Pyright para de amolar
 
-    if not isinstance(item.get("firstname"), str):
+    if not is_str(item.get("firstname")):
         return False
 
     if not isinstance(item.get("lastname"), str):
