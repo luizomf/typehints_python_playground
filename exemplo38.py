99f115609901591eee1ceb245e27c7d32df52810
Luiz Otávio <luizomf@gmail.com>
2025-08-23 12:27:23 -0300

    type guard first class

--- exemplo38.py
+++ exemplo38.py
@@ -1,64 +1,50 @@
-from collections.abc import Mapping
-from typing import Any, TypedDict, TypeGuard
+import json
+from pathlib import Path
+from typing import Any, TypedDict, TypeGuard, cast
 
-from utils import cyan_print, sep_print
+from utils import cyan_print, red_print, sep_print
 
 ################################################################################
 #
-# TypeIs e TypeGuard - Precisamos de Guarda-costas?
+# TypeGuard - Precisamos de Guarda-costas?
 #
-# Obs.: PEPs 484/742
+# Obs.: PEP 742 e 647 falam de TypeGuard e TypeIs. Focaremos em TypeGuard.
+# Depois TypeIs.
 #
-# No TypeScript, funções que fazem Type Guard, são chamadas de "Type Predicate"
-# (ou Predicado de tipos). São funções especializadas em fazer "Type Narrowing"
-# (ou afunilamento de tipo), assim como `isinstance` ou condicionais que
-# afunilam tipos no Python.
+# Comparação: No TypeScript, funções que fazem Type Guard, são chamadas de
+# "Type Predicate" (ou Predicado de tipos). São funções especializadas em fazer
+# "Type Narrowing" (ou afunilamento de tipo), assim como `isinstance` ou
+# condicionais que afunilam tipos no Python.
 #
-# `TypeIs` é um tipo usado para Type Narrowing. Criamos uma função que recebe
-# pelo menos um argumento e retorna `bool`. O Type Hint de retorno da função
-# fica como `TypeIs[T]` (onde T é o tipo afunilado). Se a função retorna `True`,
-# o tipo é afunilado para `T`. Se retornar `False`, o tipo é o mesmo do primeiro
-# argumento.
-# A função pode receber mais argumentos, porém o primeiro é onde estamos
-# trabalhando o tipo.
-# Na prática, `TypeIs[T]` deve ser consistente com `isinstance()`.
+# TypeGuard[T] e TypeIs[T] são ambas funções para "Type Predicate" no Python.
+# As duas informam ao Type Checker que se seu retorno for verdadeiro, o tipo do
+# objeto avaliado é `T`.
 #
-# Boas práticas
-#
-# - Nomeie como guard: is_*, has_*, looks_like_*.
-# - Seja sound: a função tem que garantir o tipo que promete. Nada de “achismo”.
-# - Use TypeIs para escalar/união simples; TypeGuard para refinar coleções/estruturas.
-# - Evite truques: se o retorno real é só bool, não declare TypeIs/TypeGuard.
+# Com a inclusão de TypeIs[T], TypeGuard[T] tenderá a ser menos usado, mas de
+# acordo com a PEP 742 (sobre TypeIs), TypeGuard[T] vai continuar na linguagem.
 #
 ################################################################################
 
 
-class HasFirstName(TypedDict):
+class UserDict(TypedDict):
     firstname: str
-
-
-class HasLastName(TypedDict):
     lastname: str
+    age: int
 
 
-class HasFullName(HasFirstName, HasLastName, total=False): ...
+def is_valid_dict(item: object) -> TypeGuard[UserDict]:
+    if not isinstance(item, dict):
+        return False
 
+    item = cast("dict[str, Any]", item)  # isso é para o Pyright para de amolar
 
-def has_full_name(user: Mapping[Any, Any]) -> TypeGuard[HasFullName]:
-    return bool(user.get("firstname") and user.get("lastname"))
+    if not isinstance(item.get("firstname"), str):
+        return False
 
+    if not isinstance(item.get("lastname"), str):
+        return False
 
-################################################################################
-
-
-def print_user_info(user: Mapping[Any, Any]) -> None:
-    if has_full_name(user):
-        # Tipo de user aqui é: HasFullName
-        cyan_print(user["firstname"], user["lastname"])
-        return
-
-    # Tipo de user aqui é: Mapping[Any, Any]
-    cyan_print("NOT A USER")
+    return isinstance(item.get("age"), int)
 
 
 ################################################################################
@@ -67,23 +53,17 @@ def print_user_info(user: Mapping[Any, Any]) -> None:
 if __name__ == "__main__":
     sep_print()
 
-    user = {
-        "firstname": "Luiz",
-        "lastname": "Otávio",
-        "age": 18,
-    }
-    print_user_info(user)  # Luiz Otávio
+    input_file = Path("exemplo38.json").resolve()
+    with input_file.open("r", encoding="utf8") as file:
+        user_data = json.load(file)  # Any
 
-    # if has_full_name(user):
-    #     reveal_type(user)  # HasFullName
-
-    sep_print()
-
-    any_dict = {
-        "lastname": "Otávio",
-        "age": 18,
-    }
-    print_user_info(any_dict)  # NOT A USER
+    for user in user_data:
+        if is_valid_dict(user):
+            cyan_print("✅ VALID USER")
+            cyan_print(f"{user['firstname']} {user['lastname']} {user['age']}")
+        else:
+            red_print("❌ INVALID DATA ❌")
+            red_print(user.keys())
 
     sep_print()
 
